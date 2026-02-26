5159dd00f497355408ea7ad4fbea715bd6c70f02
Deepika-Bamne <deepikabamne59@gmail.com>
2026-02-26 21:06:40 +0530

    Structure changes files and folders changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import './Headerdetials/Header.css'
-import './Homepagedetials/Homedetials.css'
+import "./App.css";
 
-import Home from './Headerdetials/Home'
-import ContactUs from './Headerdetials/ContactUs'
-import Account from './Headerdetials/Account'
-import Aboutus from "./Headerdetials/Aboutus";
-import Header from "./Headerdetials/Header";
+import Header from "./Components/Layout/Header";
+
+import Home from "./Pages/Home";
+import Aboutus from "./Pages/Aboutus";
+import ContactUs from "./Pages/ContactUs";
+
+import Account from "./Pages/Accountdetials/Account";
+import Login from "./Pages/Accountdetials/Login";
+import Createaccount from "./Pages/Accountdetials/Createaccount";
 
 function App() {
   return (
     <Router>
-          <Header/>
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<Aboutus />} />
         <Route path="/contactus" element={<ContactUs />} />
-        <Route path="/accountpage" element={<Account />} />
+
+        {/* Account Routes */}
+        <Route path="/account" element={<Account />} />
+        <Route path="/account/login" element={<Login />} />
+        <Route path="/account/create" element={<Createaccount />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
